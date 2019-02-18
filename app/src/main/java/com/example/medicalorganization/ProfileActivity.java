b9fc04afbd52d3d369144c0a23f0f81c811da911
Thomas Athanasiou <athanthomas777@gmail.com>
2019-02-18 13:30:47 +0200

    Adding Email Verification and Logout Option

--- app/src/main/java/com/example/medicalorganization/ProfileActivity.java
+++ app/src/main/java/com/example/medicalorganization/ProfileActivity.java
@@ -4,9 +4,18 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
 
 import com.example.medicalorganization.Models.Users;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -20,7 +29,7 @@ public class ProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private FirebaseDatabase mFireDatabase;
     private DatabaseReference mReference;
-    private TextView name, identity, rantevou, mail;
+    private TextView name, identity, rantevou, mail, verified;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,6 +38,10 @@ public class ProfileActivity extends AppCompatActivity {
         name = (TextView)findViewById(R.id.nameTextView);
         identity = (TextView)findViewById(R.id.identityTextView);
         mail = (TextView)findViewById(R.id.mailTextView);
+        verified = (TextView) findViewById(R.id.textViewVerified);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         mAuth = FirebaseAuth.getInstance();
         mFireDatabase = FirebaseDatabase.getInstance();
         mReference = mFireDatabase.getReference().child("Users");
@@ -49,6 +62,24 @@ public class ProfileActivity extends AppCompatActivity {
                         identity.setText(user.Identity);
                         mail.setText(user.Email);
                     }
+                    //Check if user is verified email address, if not click to verify
+                    if(mAuth.getCurrentUser().isEmailVerified()){
+                        verified.setText("Email Verified");
+                    }
+                    else {
+                        verified.setText("Email not Verified");
+                        verified.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                mAuth.getCurrentUser().sendEmailVerification().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        Toast.makeText(ProfileActivity.this, "Verification email Sent", Toast.LENGTH_SHORT).show();
+                                    }
+                                });
+                            }
+                        });
+                    }
                 }
             }
 
@@ -60,6 +91,27 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.menuLogout:
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                startActivity(new Intent(this, LoginActivity.class));
+                break;
+        }
+        return true;
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         //check if the user is not logged in
@@ -67,10 +119,10 @@ public class ProfileActivity extends AppCompatActivity {
             finish();
             startActivity(new Intent(this, LoginActivity.class));
         }
-        else {
-
-        }
     }
 
-
+    @Override
+    public void onBackPressed() {
+        startActivity(new Intent(this,LoginActivity.class));
+    }
 }
